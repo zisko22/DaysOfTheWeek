eb284593321917c20bf5173f131f53291b719546
Zisko22 <zisko22@gmail.com>
2018-04-30 17:51:58 -0700

    second commit

--- Days/Program.cs
+++ Days/Program.cs
@@ -4,9 +4,29 @@ namespace Days
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the current day of the week: ");
+            string userInput = Console.ReadLine();
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
         }
+    }
+
+    public enum Week
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
 }
